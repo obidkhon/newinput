9fa9b6e1f54c70935ae25109d41d5edf65d06c71
obidkon <obid.sharipov.80@mail.ru>
2020-11-25 22:29:54 +0500

    input

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+import ForimMake from './components/ForimMake';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+<ForimMake/>
+
+
+
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/ForimMake.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+export class ForimMake extends Component {
+
+
+constructor(props) {
+    super(props)
+
+    this.state = {
+         username:'',
+         coments:'',
+         topics:'react'
+    }
+}
+
+
+handlerUserNameChange=(event)=>{
+    this.setState({
+        username:event.target.value
+    })
+}
+comentsChange=(event)=>{
+    this.setState({
+        coments: event.target.value
+    })
+}
+
+handleTopicsChange=(event)=>{
+    this.setState({
+        topics:event.target.value
+    })
+}
+
+
+
+handleSubmit= event =>{
+    alert(`${this.state.username} ${this.state.coments} ${this.state.topics}`)
+event.preventDefault()
+}
+
+
+    render() {
+
+        const{ username,coments,topics}=this.state
+        return (
+            <form   onSubmit={this.handleSubmit}  > 
+
+<div>
+    <lable> UsserName </lable>
+    <input  typen='text'  value={username}  onChange={this.handlerUserNameChange}/>
+                 
+            </div>
+
+            <div>
+                <lable>Massage</lable>
+                <textarea type='coments' value={coments}  onChange={this.comentsChange}> </textarea>
+            </div>
+
+<div>  
+<lable> Topics</lable>
+<select value={topics} onChange={this.handleTopicsChange}>
+<option value='react'> React   </option>
+    <option value='java'> java  </option>
+    <option value='Js'> js  </option>
+  
+
+</select>
+<button type='submit'>sumit  </button>
+</div>
+
+
+            </form>
+        )
+    }
+}
+
+export default ForimMake
